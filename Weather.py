43a26d51131bd43a524fd682313712e31dc31fc5
Mgboyarei <146760160+Mgboyarei@users.noreply.github.com>
2024-02-20 20:33:21 +0000

    Written code

---
+++ Weather.py
@@ -0,0 +1,18 @@
+from random import choice
+
+percent_rain = [32, 95, 94, 78, 30, 49, 10, 30, 90, 40, 67]
+
+resps = []
+
+x = choice(percent_rain)
+print(x)
+
+if x > 90:
+    resps.append("Bring an umbrella.")
+elif x > 80:
+    resps.append("Good for the flowers?")
+elif x > 50:
+    resps.append("Watch out for clouds!")
+else:
+    resps.append("Nice day!")
+print(resps)
